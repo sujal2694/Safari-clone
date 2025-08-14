f134ee563fd40fc36374067823a611853c34a774
Sujal Valsur <sujalvalsur11@gmail.com>
2025-08-15 00:04:37 +0530

    update by author

--- script.js
+++ script.js
@@ -11,5 +11,42 @@ closeBtn.onclick = () => {
    cartSlide.style.left = '-100%';
 }
 
+// add to cart functionality
 
+let addTocartBtns = document.querySelectorAll('.cart-btn');
+
+addTocartBtns.forEach(button => {
+   button.addEventListener('click', (e) => {
+      let productBox = e.target.closest('.product');
+      addTocart(productBox);
+   });;
+});
+
+
+let addTocart = (productBox) =>{
+
+   let productName = productBox.querySelector('h1').textContent;
+   let productImg = productBox.querySelector('img').src;
+   let ProductPrice = productBox.querySelector('h2').textContent;
+
+   let cartContent = document.querySelector('.cart-box');
+   let cartBox = document.createElement('div');
+   cartBox.classList.add('product');
+   cartBox.innerHTML = `
+            <a href="#"><img src="${productImg}" alt=""></a>
+            <div class="product-details">
+                <p>Safari</p>
+                <h1>${productName}</h1>
+                <h2>${ProductPrice}</h2>
+            </div>
+            <div class="remove-btn">
+              <i class="fa fa-trash"></i>
+            </div>
+   `
+
+   cartContent.appendChild(cartBox);
+   document.querySelector('.remove-btn').onclick = () => {
+      cartContent.remove(cartBox)
+   }
+}
 
