deedb48097284d3a8f259d1f58c36e190290090f
Sujal Valsur <sujalvalsur11@gmail.com>
2025-08-14 11:03:05 +0530

    update by author

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.querySelector('#cart-btn').addEventListener('click', () => {
-   document.querySelector("#cart").classList.add('./cart-open');
+const cartSlide = document.getElementById("cart");
+const cartBtn = document.getElementById("cart-btn");
+
+cartBtn.addEventListener('click', (e) => {
+   cartSlide.style.left = '0';
 });
+
+const closeBtn = document.getElementById("close-btn");
+
+closeBtn.onclick = () => {
+   cartSlide.style.left = '-100%';
+}
+
+
+
